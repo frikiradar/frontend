6b0baa53df55fd03833c2177f8c20e2b26f5d9c5
Alberto Rodríguez Merino <alberto.rodriguez@altafonte.com>
2019-07-31 21:59:28 +0200

    updates and fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -105,7 +105,7 @@ export class AppComponent {
             navigator["app"].exitApp();
         }
       } else {
-        this.nav.back();
+        // this.nav.back();
         this.backButtonCount = 0;
       }
     });
