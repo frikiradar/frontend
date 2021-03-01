72436a599b3e520b0674d0d615b109d7cf5730d9
Alberto Rodriguez <alberto.rodriguez@altafonte.com>
2021-02-28 19:07:28 -0600

    fix bans messages

--- src/app/admin/banned-users/banned-messages/banned-messages.modal.ts
+++ src/app/admin/banned-users/banned-messages/banned-messages.modal.ts
@@ -33,6 +33,7 @@ export class BannedMessagesModal implements OnInit {
 
   public user: User;
   messages: Chat[] = [];
+  public countDownString = "";
 
   constructor(
     private modal: ModalController,
@@ -49,6 +50,11 @@ export class BannedMessagesModal implements OnInit {
   async ngOnInit() {
     this.user = await this.userSvc.getUser(this.id);
     this.messages = (await this.admin.getBannedMessages(this.id)).reverse();
+    if (this.user.ban_end) {
+      setInterval(() => {
+        this.countDown();
+      }, 1000);
+    }
   }
 
   async sendMessage(event?: Event) {
@@ -90,6 +96,25 @@ export class BannedMessagesModal implements OnInit {
     setTimeout(() => {
       this.chatlist.scrollToBottom(0);
     }, delay);
+  }
+
+  countDown() {
+    const countDownDate = new Date(this.user.ban_end).getTime();
+    var now = new Date().getTime();
+
+    // Find the distance between now and the count down date
+    var distance = countDownDate - now;
+
+    // Time calculations for days, hours, minutes and seconds
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    this.countDownString =
+      days + "d " + hours + "h " + minutes + "m " + seconds + "s ";
   }
 
   close() {
